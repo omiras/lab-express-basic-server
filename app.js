35d2705538424ee29cf7f5b15edcacc156041dbd
omiras <oscar.miras@gmail.com>
2024-07-01 21:33:14 +0200

    iteration 3

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 // IMPORT PACKAGES
 // Here you should import the required packages for your Express app: `express` and `morgan`
+const express = require('express');
+const logger = require('morgan');
 
 
 
 // CREATE EXPRESS APP
 // Here you should create your Express app:
+const app = express();
+
 
 
 
@@ -13,13 +17,21 @@
 // - `express.static()` to serve static files from the `public` folder
 // - `express.json()` to parse incoming requests with JSON payloads
 // - `morgan` logger to log all incoming requests
-
+app.use(express.static('public'));
+app.use(express.json());
+app.use(logger('tiny'));
 
 
 // ROUTES
 // Start defining your routes here:
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html');
+});
 
 
 
 // START THE SERVER
 // Make your Express server listen on port 5005:
+app.listen(5005, () => {
+    console.log(' Escuchando peticiones en el puerto 5005');
+})
